9ec656b6dca232556c98aa1b3cec5e5fe1f4f748
JayShortway <29483617+JayShortway@users.noreply.github.com>
2025-06-13 16:55:59 +0200

    Passing parameters as env vars.

--- src/scripts/set-ssh-key-for-git.sh
+++ src/scripts/set-ssh-key-for-git.sh
@@ -8,8 +8,8 @@ for key in ~/.ssh/*; do
         echo "Checking $key"
         if sha256=$(ssh-keygen -lf "$key" -E sha256 2>/dev/null | awk '{print $2}' 2>/dev/null); then
             echo "SHA256: $sha256"
-            echo "Expected: << parameters.fingerprint >>"
-            if [ "$sha256" = "<< parameters.fingerprint >>" ]; then
+            echo "Expected: $PARAMETERS_FINGERPRINT"
+            if [ "$sha256" = "$PARAMETERS_FINGERPRINT" ]; then
                 echo "export GIT_SSH_COMMAND=\"ssh -i $key\"" >> "$BASH_ENV"
                 key_found=true
                 break
@@ -18,6 +18,6 @@ for key in ~/.ssh/*; do
     fi
 done
 if [ "$key_found" = false ]; then
-    echo "Error: SSH key with fingerprint << parameters.fingerprint >> not found"
+    echo "Error: SSH key with fingerprint $PARAMETERS_FINGERPRINT not found"
     exit 1
 fi
