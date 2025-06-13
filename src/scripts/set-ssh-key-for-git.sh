61b0aed27ed478eddbb5657b674ec4edf262c41c
JayShortway <29483617+JayShortway@users.noreply.github.com>
2025-06-13 16:03:40 +0200

    debugging

--- src/scripts/set-ssh-key-for-git.sh
+++ src/scripts/set-ssh-key-for-git.sh
@@ -5,7 +5,10 @@ set -euo pipefail
 key_found=false
 for key in ~/.ssh/*; do
     if [ -f "$key" ]; then
+        echo "Checking $key"
         if sha256=$(ssh-keygen -lf "$key" -E sha256 2>/dev/null | awk '{print $2}' 2>/dev/null); then
+            echo "SHA256: $sha256"
+            echo "Expected: << parameters.fingerprint >>"
             if [ "$sha256" = "<< parameters.fingerprint >>" ]; then
                 echo "export GIT_SSH_COMMAND=\"ssh -i $key\"" >> "$BASH_ENV"
                 key_found=true
