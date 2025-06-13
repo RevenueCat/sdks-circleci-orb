09d90c505721742a4ff579a934ec53391bdca9e7
JayShortway <29483617+JayShortway@users.noreply.github.com>
2025-06-13 17:39:52 +0200

    Removes some debug echos.

--- src/scripts/set-ssh-key-for-git.sh
+++ src/scripts/set-ssh-key-for-git.sh
@@ -5,10 +5,7 @@ set -euo pipefail
 key_found=false
 for key in ~/.ssh/*; do
     if [ -f "$key" ]; then
-        echo "Checking $key"
         if sha256=$(ssh-keygen -lf "$key" -E sha256 2>/dev/null | awk '{print $2}' 2>/dev/null); then
-            echo "SHA256: $sha256"
-            echo "Expected: $PARAMETERS_FINGERPRINT"
             if [ "$sha256" = "$PARAMETERS_FINGERPRINT" ]; then
                 echo "export GIT_SSH_COMMAND=\"ssh -i $key\"" >> "$BASH_ENV"
                 key_found=true
